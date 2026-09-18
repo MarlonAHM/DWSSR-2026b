9e8147682f7772f422b13294d19656a1bc696d57
Herrera <marlonherrera2004@gmail.com>
2026-09-18 18:17:11 +0000

    ✨ Feat: Enhance app.js Enhance app.js With middleware configuration and error handling comments; update www for debug message; add greet script in package.json; include itgam image

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
+//Funcion para manejar errores 
 var createError = require('http-errors');
+//Importa el framework express
 var express = require('express');
+//Importa modulos para manejar rutas 
 var path = require('path');
+//Importa modulos paa manejar cookies 
 var cookieParser = require('cookie-parser');
+//Importa modulos para manejar logs 
 var logger = require('morgan');
 
+//Importar las rutas de la aplicacion 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//Crea la aplicacion express
 var app = express();
 
-// view engine setup
+//Congigurar el motor de vistas 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//Configurar middlewares de la aplicacion
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//Configurar la carpeta publica para servir archivos estaticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Registrar las rutas de la aplicacion
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+//Capturar errores 404 y enviarlos al manejador de errores
 app.use(function(req, res, next) {
   next(createError(404));
 });
